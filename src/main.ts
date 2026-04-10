349e45eda02e91deb5833b42e6b9828058b1c33f
Ivan Caviedes <ivancaviedes99@gmail.com>
2026-04-10 12:54:37 -0500

    chore: setup eslint and prettier configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { LoggerService } from './app/core/services/logger.service';
+
+const logger = new LoggerService();
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -11,4 +19,4 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
-});
+}).catch((err) => logger.error('App bootstrap failed', err));
