093489dc25b716e2e09d8abe3e1c98c1da59a49d
Ivan Caviedes <ivancaviedes99@gmail.com>
2026-04-10 12:15:45 -0500

    chore: initial project setup with ionic angular template

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.seti.nequi.notes',
+  appName: 'NoteFlow',
+  webDir: 'www',
+};
+
+export default config;
