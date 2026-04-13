15e66dc523c5bccabe56de48b20da5c9a56250f2
Ivan Caviedes <ivancaviedes99@gmail.com>
2026-04-13 00:30:56 -0500

    feat: add build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ' ',
+    apiKey: 'AIzaSyDE2ok-BbWZhA8Q5yjnWykvDu070fI7d80',
+    authDomain: 'test-notas-f17e3.firebaseapp.com',
+    projectId: 'test-notas-f17e3',
+    storageBucket: 'test-notas-f17e3.firebasestorage.app',
+    messagingSenderId: '534620086898',
+    appId: '1:534620086898:web:cce916a64a7f46374458eb',
   },
 };
